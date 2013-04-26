e9b64f6f86c201a254670b0b81da14affc82ab79
Toralf Wittner <tw@dtex.org>
2013-04-26 17:49:26 +0200

    Add internal 'replace' function.

--- src/Snap/Predicates/Internal.hs
+++ src/Snap/Predicates/Internal.hs
@@ -1,14 +1,19 @@
+{-# LANGUAGE BangPatterns #-}
 module Snap.Predicates.Internal
   ( headers
   , params
   , safeHead
+  , replace
   )
 where
 
 import Snap.Core hiding (headers)
 import Data.ByteString (ByteString)
 import Data.CaseInsensitive (mk)
+import Data.Char (ord)
+import Data.Monoid
 import qualified Data.Map.Strict as M
+import qualified Data.ByteString as S
 
 headers :: Request -> ByteString -> [ByteString]
 headers rq name = maybe [] id . getHeaders (mk name) $ rq
@@ -19,3 +24,11 @@ params rq name = maybe [] id . M.lookup name . rqParams $ rq
 safeHead :: [a] -> Maybe a
 safeHead []    = Nothing
 safeHead (h:_) = Just h
+
+replace :: Char -> ByteString -> ByteString -> ByteString
+replace !c !r !s
+    | S.null s  = S.empty
+    | otherwise = let (h, t) = S.break (== w) s
+                  in h <> r <> (replace c r (S.drop 1 t))
+  where
+    w = fromIntegral (ord c)
